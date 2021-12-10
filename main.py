dfa13d38659eec78c12751eb736e313cbb4df9ca
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2021-12-10 17:20:15 +0330

    persian punctuation characters added. tokenizer example improved.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Stemmer import Stemmer
 from Normalizer import Normalizer
 from Tokenizer import Tokenizer
+
 stemmer = Stemmer()
 print(stemmer.stem("گفتۀ"))
 print(stemmer.stem("نویسنده هایش"))
@@ -10,4 +11,4 @@ print(normalizer.normalize("123 علیﻚ سلام"))
 print(normalizer.normalize("كي به کی"))
 
 tokenizer = Tokenizer()
-print(tokenizer.tokenize("كي به کی"))
+print(tokenizer.tokenize("علی و حسن، به مدرسه رفتند. و در راه بازگشت به خانه دوستانِ قدیمی شان را دیدند. آیا آنها خوشحال شدند؟ یا خیر؟!"))
