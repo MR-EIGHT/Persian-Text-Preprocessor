b000c96e78a55e52464ddaaae0f6131aa9bd1656
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2021-12-10 17:04:28 +0330

    tokenizer implemented.

---
+++ Tokenizer.py
@@ -0,0 +1,23 @@
+class Tokenizer:
+
+    def __init__(self):
+        self.english_punctuations = {'.', ',', ':', ';', '!', '#', '@', '$', '%', '^', '&', '*', '(', ')', '"', "'"}
+        self.persian_punctuations = {'.', '،', ':', '؛', '!', '#', '@', '$', '%', '^', '&', '*', '(', ')', '"', "'",
+                                     "«", "»", "٪"}
+        self.tokens = []
+
+    def tokenize(self, text):
+        tok = []
+        for i in range(0, len(text)):
+            if not self.__issplitter__(text[i]):
+                tok.append(text[i])
+            elif len(tok) > 0:
+                self.tokens.append("".join(tok))
+                tok.clear()
+        if len(tok) > 0:
+            self.tokens.append("".join(tok))
+
+        return self.tokens
+
+    def __issplitter__(self, char):
+        return char in self.persian_punctuations or char in self.persian_punctuations or char.isspace()
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from Stemmer import Stemmer
 from Normalizer import Normalizer
-
+from Tokenizer import Tokenizer
 stemmer = Stemmer()
 print(stemmer.stem("گفتۀ"))
 print(stemmer.stem("نویسنده هایش"))
 
-nor = Normalizer()
-print(nor.normalize("123 علیﻚ سلام"))
-print(nor.normalize("كي به کی"))
+normalizer = Normalizer()
+print(normalizer.normalize("123 علیﻚ سلام"))
+print(normalizer.normalize("كي به کی"))
 
+tokenizer = Tokenizer()
+print(tokenizer.tokenize("كي به کی"))
