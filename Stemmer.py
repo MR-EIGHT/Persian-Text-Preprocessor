e8f316d0572706f2ca826247a63f6698f8ef1613
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2021-12-13 23:57:22 +0330

    list stemming added. tokenizer improved. normalizer character list updated.

--- Stemmer.py
+++ Stemmer.py
@@ -1,15 +1,16 @@
 class Stemmer:
 
     def __init__(self):
-        self.ends = ['ات', 'ان', 'ترین', 'تر', 'م', 'ت', 'ش', 'یی', 'ی', 'ها', 'ٔ', '‌ا', '‌', 'ۀ']
+        self.ends = ['ات', 'گان', 'ان', 'ترین', 'تر', 'م', 'ت', 'ش', 'یی', 'ی', 'ها', 'ٔ', '‌ا', '‌', 'ۀ']
         self.verb_ends = ['ام', 'ای', 'ایم', 'اید', 'اند']
 
     def stem(self, word):
         if len(word) < 4:
             return word.strip()
-        for end in self.ends:
+        for end in self.verb_ends + self.ends:
             if word.endswith(end):
                 word = word[:-len(end)]
+
         return word.strip()
 
     def list_stem(self, word_set):
--- main.py
+++ main.py
@@ -5,15 +5,23 @@ from Tokenizer import Tokenizer
 stemmer = Stemmer()
 print(stemmer.stem("گفتۀ"))
 print(stemmer.stem("نویسنده هایش"))
+print(stemmer.stem("نویسندگان"))
+print(stemmer.stem("ستودگان"))
+print(stemmer.stem("رفته ایم"))
+
 
 normalizer = Normalizer()
 print(normalizer.normalize("123 علیﻚ سلام"))
 print(normalizer.normalize("كي به کی"))
+
+
 
 tokenizer = Tokenizer()
 print(tokenizer.tokenize(
     "علی و حسن، به مدرسه رفتند. و در راه بازگشت به خانه دوستانِ قدیمی شان را دیدند. آیا آنها خوشحال شدند؟ یا خیر؟! "
     "می‌خواهم بدانم."))
 
-d = open("data.xml", "r", encoding='utf-8')
-print(stemmer.list_stem(tokenizer.tokenize(d.read(), True)))
+
+
+# d = open("data.xml", "r", encoding='utf-8')
+# print(stemmer.list_stem(tokenizer.tokenize(d.read(), True)))
