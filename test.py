e5f269418e4d945ce7c79c7156d18f96c7ddeac2
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2021-12-21 03:13:17 +0330

    renamed main to test.

--- test.py
+++ test.py
@@ -19,11 +19,4 @@ print(tokenizer.tokenize(
     "علی و حسن، به مدرسه رفتند. و در راه بازگشت به خانه دوستانِ قدیمی شان را دیدند. آیا آنها خوشحال شدند؟ یا خیر؟! "
     "می‌خواهم بدانم."))
 
-# wiki = Wikipedia("./data/fawiki-20211101-abstract.xml")
-# print(len(wiki.doc_store))
-
-
-
-
-
 
