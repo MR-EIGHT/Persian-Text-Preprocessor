aeb205541c144c1b8ab8e75c9e68a557e387384f
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2021-12-21 12:50:13 +0330

    wikipedia renamed.

--- preper/__init__.py
+++ preper/__init__.py
@@ -1,5 +1,5 @@
 from .Tokenizer import Tokenizer
 from .Stemmer import Stemmer
 from .Normalizer import Normalizer
-from .wikipedia import Wikipedia
+from .Wikipedia import Wikipedia
 from .Document import Document
